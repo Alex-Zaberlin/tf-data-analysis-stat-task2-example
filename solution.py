f6026796dadf3d575299f5d7ca82f58a4c4cea8e
Aleksey Zabelin <55365897+Alex-Zaberlin@users.noreply.github.com>
2023-04-05 21:59:11 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import norm
-
+from scipy.stats import expon
 
 chat_id = 282219367 # Ваш chat ID, не меняйте название переменной
 
@@ -11,7 +11,7 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    loc = 1
+    scale = 1
+    return (x.mean() - 1/2 + expon.ppf(1 - alpha, loc=loc, scale=scale))/35**2, \
+            (x.mean() - 1/2 - expon.ppf(alpha, loc=loc, scale=scale))/35**2
