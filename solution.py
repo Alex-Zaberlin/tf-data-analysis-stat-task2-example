546fec1310197c5439addb8b250e015799208432
Aleksey Zabelin <55365897+Alex-Zaberlin@users.noreply.github.com>
2023-04-06 01:41:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
+from math import log
 
 from scipy.stats import norm
 from scipy.stats import expon
+
 
 chat_id = 282219367 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
-    loc = 1
-    scale = 1
-    return (x.mean() - 1/2 + expon.ppf(1 - alpha, loc=loc, scale=scale))/35**2, \
-            (x.mean() - 1/2 - expon.ppf(alpha, loc=loc, scale=scale))/35**2
+    loc = x.mean()
+    return (loc - 1/2 - log(1-alpha))*2/(35**2), \
+           (loc - 1/2 - log(alpha))*2/(35**2)
