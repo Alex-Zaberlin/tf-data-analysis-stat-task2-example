c8ffe2f6a62baa3a130cf2b9c150fde477be584e
Aleksey Zabelin <55365897+Alex-Zaberlin@users.noreply.github.com>
2023-04-06 20:58:48 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,7 +9,6 @@ from scipy.stats import expon
 chat_id = 282219367 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p
-    loc = x.mean()
-    return (loc - 1/2 - log(1-alpha))*2/(35**2), \
-           (loc - 1/2 - log(alpha))*2/(35**2)
+    alpha = p
+    time = 35
+    return (2 * np.mean(x) / time ** 2 -  norm.ppf(1 - alpha / 2, scale=2) / time ** 2, 2 * np.mean(x) / time ** 2 - norm.ppf(alpha / 2, scale=2) / time ** 2)
