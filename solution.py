583aa9af6c628b2e97b33d62d7676771a439efdf
Aleksey Zabelin <55365897+Alex-Zaberlin@users.noreply.github.com>
2023-04-06 22:14:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,6 +6,7 @@ from scipy.stats import norm
 chat_id = 282219367 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    alpha = p
-    time = 35
-    return (2 * np.mean(x) / time ** 2 -  norm.ppf(1 - alpha / 2, scale=2) / time ** 2, 2 * np.mean(x) / time ** 2 - norm.ppf(alpha / 2, scale=2) / time ** 2)
+    a = ((x+0.5)*2)/35**2
+    alpha = 1 - p
+    return a.mean() - np.sqrt(np.var(a)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(a)), \
+           a.mean() - np.sqrt(np.var(a)) * norm.ppf(alpha / 2) / np.sqrt(len(a))
