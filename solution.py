58c25cf2d3ca66fffbb1673afe20749ac9ef879f
Aleksey Zabelin <55365897+Alex-Zaberlin@users.noreply.github.com>
2023-04-06 22:55:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,7 +7,8 @@ from scipy.stats import expon
 chat_id = 282219367 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    a = ((x+0.5)*2)/35**2
+    t = 35
     alpha = 1 - p
-    return a.mean() - np.sqrt(np.var(a)) * expon.ppf(1 - alpha / 2) / np.sqrt(len(a)), \
-           a.mean() - np.sqrt(np.var(a)) * expon.ppf(alpha / 2) / np.sqrt(len(a))
+    min = (-x).min()
+    z_2 = -np.log(1-p)/x.size    
+    return 2*(-min-1/2)/(t*t), 2*(z_2-min-1/2)/(t*t)
